a2c7bd10176424cddfd1d7d81c6026e4fc7741e5
armageddonarmadillo <ariel@aandcorp.com>
2022-04-17 00:52:02 -0400

    fixed reference logic for screenshot builder, added random text from typicode

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import axios from 'axios'
-import { useEffect, useState, useRef, createRef } from 'react'
+import { useEffect, useState, createRef } from 'react'
 import { styles } from './styles'
 import { exportComponentAsPNG } from 'react-component-export-image'
 
 const githuburl = 'https://api.github.com/users'
+const typicodeurl = 'https://jsonplaceholder.typicode.com/comments'
 
 const App = () => {
 
-  const imgref = createRef();
+  const imgref = new Map();
   const [users, set_users] = useState([])
+  const [data, set_data] = useState([])
+  useEffect(() => { axios.get(githuburl).then((res) => { set_users(res.data)}).catch((err) => { console.log(err)})}, [])
+  useEffect(() => { axios.get(typicodeurl).then((res) => { set_data(res.data)}).catch((err) => { console.log(err)})}, [])
 
-  useEffect(() => {
-     axios.get(githuburl).then((res) => {
-      set_users(res.data)
-    }).catch((err) => { console.log(err)})
-  }, [])
-
+  const setRef = (id) => {
+    const ref = createRef();
+    imgref.set(id, ref);
+    return ref;
+  }
+  const getRef = (e) => {
+    console.log(users)
+    e.target.id && exportComponentAsPNG(imgref.get(parseInt(e.target.id)), {fileName:`${e.target.innerText.split('\n')[0]}.png`})
+  }
 
   return (
-    <div>
+    <div style={styles.container}>
       {users.map((u)=>{
         return (
-          <div ref={imgref} key={u.id} style={styles.card_container}>
+          <div ref={setRef(u.id)} key={u.id} id={u.id} style={styles.card_container} onClick={getRef}>
             <img src={u.avatar_url} style={styles.card_art}/>
-            <div style={styles.card_name} onClick={() => exportComponentAsPNG(imgref)}> {u.login} </div>
-            <div style={styles.card_text}> {u.html_url} </div>
+            <div style={styles.card_name}> /u/{u.login} </div>
+            <div style={styles.card_text}> {u.html_url}<br/> {data.at(u.id).body} </div>
           </div>
         )
       })}
--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,22 @@
 export const styles = {
+    container:{
+        width:'100%',
+        height:'100%',
+        display:'grid', //this 
+        gridTemplateColumns:'auto', //and this are messing up screenshots
+    },
     card_container:{
         width:'252px',
         height:'352px',
         border:'10px solid #f5d742',
         borderRadius:'8px',
         backgroundColor:'#a27e5d',
+        margin:'25px',
         padding:'10px',
-        margin:'25px'
     },
     card_art:{
         width:'99%',
-        height:'45%',
+        height:'50%',
         border:'2px solid #d8b827',
         borderRadius:'8px',
     },
@@ -20,12 +26,14 @@ export const styles = {
         textAlign:'center',
         border:'1px solid #a27e5d',
         backgroundColor:'#997453',
-        borderRadius:'50px'
+        borderRadius:'50px',
+        marginTop:'5px'
     },
     card_text:{
         fontFamily:'monospace',
         textAlign:'center',
-        marginTop:'20px',
+        fontSize:'2.55vw',
+        marginTop:'10px',
         width:'97%',
         height:'33%',
         border:'2px solid #d8b827',
